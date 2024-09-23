b54a49201f222cb4a15995922321350830761fa0
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-09-23 19:04:30 -0400

    Crear registro

---
+++ Models/Registro.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeremyDelosSantos_Ap1_P1.Models;
+
+public class Registro
+{
+    [Key]
+    public string Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+}
