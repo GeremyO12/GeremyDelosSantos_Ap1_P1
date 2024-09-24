d194889dffe605cdd3bd3f9d25cbc77fadfbea12
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-09-23 21:10:16 -0400

    Agregado

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 using GeremyDelosSantos_Ap1_P1.Components;
+using GeremyDelosSantos_Ap1_P1.DAL;
+using GeremyDelosSantos_Ap1_P1.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(ConStr));
+
+builder.Services.AddScoped<RegistroServices>();
 
 var app = builder.Build();
 
