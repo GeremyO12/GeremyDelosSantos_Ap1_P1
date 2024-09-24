c2b4221e82ddda829b98bea010d32c819fbd1fe7
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-09-23 21:09:59 -0400

    Creacion de Services

---
+++ Services/RegistroServices.cs
@@ -0,0 +1,13 @@
+﻿using GeremyDelosSantos_Ap1_P1.DAL;
+
+namespace GeremyDelosSantos_Ap1_P1.Services;
+
+public class RegistroServices(Contexto Contexto)
+{
+    //guardar
+    //buscar
+    //listar
+    //Modificar
+    //Eliminar
+    //Existe
+}
