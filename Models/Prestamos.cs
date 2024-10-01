6d1487ab81bcb8c05383a9df790f09668a321a75
Geremy De los Santos S. <geremiorlando28@hotmail.com>
2024-09-30 21:11:27 -0400

    Se creo la clase Prestamos

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -5,7 +5,12 @@ namespace GeremyDelosSantos_Ap1_P1.Models;
 public class Prestamos
 {
     [Key]
-    public string PrestamosId { get; set; }
-    [Required]
-    public string Nombre { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual que 1.")]
+    public int PrestamosId { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio.")]
+    public string Deudor { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio.")]
+    public string Concepto { get; set; }
+    [Required(ErrorMessage = "El campo es obligatorio.")]
+    public decimal Monto { get; set; }
 }
